e54e61ffe665e1147e2cf63e406f5f1e60e408d0
Jamie Maynard <jamie@punkyideas.com>
2024-08-11 19:23:29 +0100

    Update config files for Linux+Antidote

--- zsh/zsh.d/02-antidote.zsh
+++ zsh/zsh.d/02-antidote.zsh
@@ -3,12 +3,16 @@
 #source <(antibody init)
 #antibody bundle < ~/.zsh_plugins.txt
 
-source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+if [[ $(uname) == "Linux" ]]; then
+    source ~/.antidote/antidote.zsh
+else
+    source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+fi
 zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
 if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
   (
-    source /path-to-antidote/antidote.zsh
     antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
+    antidote load
   )
 fi
 source ${zsh_plugins}.zsh
